9fd4dd12411ee52a79d5c461e0068cf0e3a25902
aeris-path <82672084+aeris-path@users.noreply.github.com>
2022-06-19 20:59:04 +0200

    Add files via upload

---
+++ MuG-Web/script.js
@@ -0,0 +1,61 @@
+$('img[usemap]').imageMap();
+
+function field1(){
+    var popupover = document.getElementById("popup-over-1")
+        popupover.style.top = "8%";
+        popupover.style.left = "16%";
+    var popup = document.getElementById("popup1");
+        popup.style.top = "8%";
+        popup.style.left = "16%";
+    popup.classList.toggle("show");
+}
+function field2(){
+    var popupover = document.getElementById("popup-over-2")
+        popupover.style.top = "5%";
+        popupover.style.left = "10%";
+    var popup = document.getElementById("popup2");
+        popup.style.top = "5%";
+        popup.style.left = "10%";
+    popup.classList.toggle("show");
+}
+
+function field3(){
+    var popupover = document.getElementById("popup-over-3")
+        popupover.style.top = "5%";
+        popupover.style.left = "50%";
+    var popup = document.getElementById("popup3");
+        popup.style.top = "10%";
+        popup.style.left = "50%";
+        popup.classList.toggle("show");
+}
+function field4(){
+    var popupover = document.getElementById("popup-over-4")
+        popupover.style.top = "5%";
+        popupover.style.left = "70%";
+    var popup = document.getElementById("popup4");
+        popup.style.top = "15%";
+        popup.style.left = "70%";
+    popup.classList.toggle("show");
+}
+function field5(){
+    var popupover = document.getElementById("popup-over-5")
+        popupover.style.top = "5%";
+        popupover.style.left = "90%";
+    var popup = document.getElementById("popup5");
+        popup.style.top = "80%";
+        popup.style.left = "90%";
+    popup.classList.toggle("show");
+}
+
+function field6(){
+    var popupover = document.getElementById("popup-over-6")
+        popupover.style.top = "5%";
+        popupover.style.left = "90%";
+    var popup = document.getElementById("popup6");
+        popup.style.top = "80%";
+        popup.style.left = "90%";
+    popup.classList.toggle("show");
+}
+
+
+
